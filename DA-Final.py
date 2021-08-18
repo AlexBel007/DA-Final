06f25efa56c6ef05e48e7c99d64c59b4be4e68e6
AlexBel007 <alexbel007@gmail.com>
2021-08-18 20:44:26 +0300

    Initial commit

--- DA-Final.py
+++ DA-Final.py
@@ -14,23 +14,24 @@ import datetime
 dfLV = pandas.read_csv(".\data\covid_19_stats_LV.csv", delimiter=";", encoding="UTF8")
 dfLT = pandas.read_csv(".\data\COVID19_stats_LT.csv", delimiter=",", encoding="UTF8")
 
-# %% clean LV dataset
+
 
 columnsToDrop = ["TestuSkaits","Ipatsvars","IzarstetoPacientuSkaits","ApstiprinatiVecGr_0-9Gadi","ApstiprinatiVecGr_10-19Gadi","ApstiprinatiVecGr_20-29Gadi","ApstiprinatiVecGr_30-39Gadi","ApstiprinatiVecGr_40-49Gadi","ApstiprinatiVecGr_50-59Gadi","ApstiprinatiVecGr_60-69Gadi","ApstiprinatiVecGr_70GadiUnVairak","ApstiprinatiVecGr_70-79Gadi","ApstiprinatiVecGr_80GadiUnVairak","14DienuKumulativaSaslimstibaUz100000Iedzivotaju", "ApstCOVID19InfSk_NevakcVakcNepab", "ApstCOVID19InfSk_Vakc"]
 
+#remove columns
 dfLV.drop(columnsToDrop, 1, inplace=True)
-
 
 dfLV.rename(columns={"ApstiprinataCOVID19InfekcijaSkaits": "NewInfections",
                 "MirusoPersonuSkaits": "Deaths",
                 "Datums": "DateValue",
                 "IzveselojusosSkaits": "Recovered"}, inplace=True)
 
-
-
-#remove columns
 # %%
-dfLV["DateValue"].apply(lambda x: datetime.datetime.strptime(x, "%Y.%m.%d."), result_type='broadcast')
+dfLV["DateValue"] = dfLV["DateValue"].apply(lambda x: datetime.datetime.strptime(x, "%Y.%m.%d."), result_type='broadcast')
 
 #datetime.datetime.strptime("2020.03.02.", "%Y.%m.%d.")
-# %%
+# %% clean LT dataset
+
+# %% Add forecasted range
+
+# %% Visualize
